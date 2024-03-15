7ab272e824d39330534c01759a7f004365a444a1
Roman <gosahorosij328@gmail.com>
2024-03-15 16:58:12 +0300

    save user: mongodb

--- backend/index.js
+++ backend/index.js
@@ -8,17 +8,21 @@ import UserModel from "./models/user.js";
 
 const PORT = 5000;
 const app = express();
+
 //path to mongodb*  путь к: mongodb
 const uri =
-  "mongodb+srv://romanadmin:2702644a@cluster0.brffnzm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  "mongodb+srv://romanadmin:2702644a@cluster0.brffnzm.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0";
+
 //processing connection to: mongodb*  обработка соединения с: mongodb
 mongoose
   .connect(uri)
   .then(() => console.log("DB ok!"))
   .catch((err) => console.log("DB error", err));
+
 //middleware_function*  связующее программное обеспечение
 app.use(express.json());
 app.use(express.urlencoded());
+
 //POST: auth redister*  регистрация авторизации
 app.post("/auth/redister", registerValidation, async (req, res) => {
   //Validation*  результат проверки: express-validator(в папке: ./validations/auth.js )
@@ -30,7 +34,7 @@ app.post("/auth/redister", registerValidation, async (req, res) => {
   const password = req.body.password;
   const salt = await bcrypt.genSalt(10);
   const passwordHash = await bcrypt.hash(password, salt);
-  //created a user model*  создал модель пользователя
+  //created a user model*  создал модель пользователя(модель в папке: ./models/user.js)
   const doc = new UserModel({
     email: req.body.email,
     fullName: req.body.fullName,
@@ -40,8 +44,9 @@ app.post("/auth/redister", registerValidation, async (req, res) => {
   //save the user*  сохраняем пользователя
   const user = await doc.save();
   //response to client*  ответ клиенту
-  res.json({ success: true });
+  res.json(user);
 });
+
 // start server* запускает сервер
 app.listen(PORT, (err) => {
   if (err) return console.log(err);
